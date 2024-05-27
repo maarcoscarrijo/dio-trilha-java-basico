5637c254974ef3aa9eb81e00f60f2fb93fe87104
maarcoscarrijo <carrijo.x@hotmail.com>
2024-05-27 09:51:50 -0300

    feat: Implementado o Scanner para entrada dos dados, e Print para apresentação.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner contaCorrente = new Scanner(System.in);
+
+        System.out.println("Por favor, entre com o número da Conta Corrente");
+        int conta = contaCorrente.nextInt();
+
+        System.out.println("Por favor, entre com o número da Agência");
+        String agencia = contaCorrente.next();
+
+        System.out.println("Digite o nome completo do titular da conta");
+        String nome = contaCorrente.next();
+
+        System.out.println("Digite o saldo em conta");
+        float saldo = contaCorrente.nextFloat();
+        
+        System.out.println("Olá " + nome + ", obrigado por criar sua conta em nosso banco.");
+        System.out.println("Sua agência é: " + agencia + ", conta " + conta + " e o saldo é R$" + saldo + "!");
+        
+        contaCorrente.close();
     }
 }
